1d32134f3bd30df1ca560a44d900ab918d9d5934
unknown <you@example.com>
2021-07-06 12:17:13 +0300

    kartejdan foydalanish

---
+++ Random dasturi/Massivlar.py
@@ -0,0 +1,34 @@
+a='123456'
+b=123
+sonlar=[1, 2, 3, 'salom', True, [1, 2, 3]]
+print(type(sonlar))
+
+mashinalar=['nexia', 'Spark', 'Damas']
+mashinalar [0]="Cobalt"
+print(mashinalar)
+
+uquvchilarRuyxati=['Alisher', 'Sardor', 'Eldor']
+uquvchilarRuyxati1=['Eldor', 'Shodiyor', 'Olim']
+uquvchilarRuyxati [2]="Diyor"
+print(uquvchilarRuyxati)
+uquvchilarRuyxati=['Alisher', 'Sardor', 'Eldor']
+uquvchilarRuyxati.append("Farxod")
+print(uquvchilarRuyxati)
+uquvchilarRuyxati.clear()
+print(uquvchilarRuyxati)
+uquvchilarRuyxati.extend(uquvchilarRuyxati1)
+print(uquvchilarRuyxati1)
+
+mashinalar=['Nexia', 'Lasetti', 'Lasetti', 'Spark']
+mashinalar.count("Lasetti")
+mashinalar.remove("Lasetti")
+mashinalar.remove("Lasetti")
+print(mashinalar)
+
+sonlar=[1, 2, 3, 4, 5, 6]
+for son in sonlar:
+    if son % 2==0:
+        print(son)
+
+
+
